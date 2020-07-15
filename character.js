ff125d27edf648480729660e482018d303c67d2b
Tripathikritika <kritikaktripathi@gmail.com>
2020-07-15 19:41:31 +0530

    Structing Character Page

--- character.js
+++ character.js
@@ -1,8 +1,8 @@
 window.addEventListener('load',function(){
-    getCharacter()
+   handleGet()
 })
 
-function getCharacter(){
+function handleGet(){
     //alert("pop")
     // var query = document.getElementById('os').value
    var xhr = new XMLHttpRequest()
@@ -12,6 +12,8 @@ function getCharacter(){
       if(xhr.status === 200){
          var res = JSON.parse(xhr.response)
          console.log(res)
+         getCharacter(res)
+        
       }
       else{
          var div = document.createElement('div')
@@ -19,4 +21,47 @@ function getCharacter(){
       }
    }
 
+}
+
+function getCharacter(data){
+   var items  = data.results
+   var displayResult = document.getElementById('charResult')
+   displayResult.textContent = ""
+
+   for(var i = 0 ; i < items.length ; i++){
+      var card = document.createElement('div')
+      card.setAttribute('class','card m-3 col-lg-6')
+
+      var cardRow = document.createElement('div')
+      cardRow.setAttribute('class','row no-gutters')
+
+      var cardCol = document.createElement('div')
+      cardCol.setAttribute('class','col-md-4')
+      var  img = document.createElement('img')
+      img.setAttribute('src',items[i].image)
+      img.setAttribute('class','card-img')
+      
+      cardCol.append(img)
+
+      var cardBody = document.createElement('div')
+      cardBody.setAttribute('class','card-body col-md-8')
+
+      var namePara = document.createElement('p')
+      namePara.textContent = "Name: "+items[i].name
+
+      var DatePara = document.createElement('p')
+      DatePara.textContent = "Gender: "+items[i].gender
+
+      var statusPara = document.createElement('p')
+      statusPara.textContent = "Status: "+items[i].status
+
+      var speciesPara = document.createElement('p')
+      speciesPara.textContent = "Species: "+items[i].species
+
+      cardBody.append(namePara,DatePara,statusPara,speciesPara)
+      cardRow.append(cardCol,cardBody)
+      card.append(cardRow)
+      displayResult.append(card)     
+
+   }
 }
