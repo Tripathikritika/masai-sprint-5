c8a9670bf7224f1028ae4caffdc12e2875e1b596
Tripathikritika <kritikaktripathi@gmail.com>
2020-07-16 13:59:37 +0530

    Character Page

--- character.js
+++ character.js
@@ -1,89 +1,46 @@
-var query = ""
+var NameQuery = ""
 var activePage = 1
 window.onload=function(){
    var searchBar = document.querySelector('.search-container')
    searchBar.addEventListener('keyup',debouncer(1000,getSingleChar))
-   searchBar.addEventListener('dblclick',handleGet)
+//    searchBar.addEventListener('dblclick',handleGet)
 }
 
 function debouncer(delay,callback){
    var debounce;
    return function(){
-      query = event.target.value
+      NameQuery = event.target.value
       debounce && clearTimeout(debounce)
       debounce = setTimeout(function(){
          activePage = 1
-         callback(query)
+         console.log(NameQuery)
+         callback(NameQuery)
       },delay)
    }
 }
 
-function handleGet(){
-    //alert("pop")
+function getSingleChar(NameQuery){
+  
+    var url;
+    if(NameQuery=="Alive" || NameQuery=="Dead" || NameQuery=="Unknown"){
+        url = "https://rickandmortyapi.com/api/character/?status=" + NameQuery
+    }
+    else if(NameQuery=="Show All"){
+      url = "https://rickandmortyapi.com/api/character/" 
+    }
+    else if(NameQuery=="Female" || NameQuery=="Male" || NameQuery=="Genderless" || NameQuery=="Unknown"){
+      url = "https://rickandmortyapi.com/api/character/?gender=" + NameQuery 
+    }
+    else if(NameQuery=="Human" || NameQuery=="Alien" ){
+      url = "https://rickandmortyapi.com/api/character/?species=" + NameQuery 
+    }
+    else{
+        url = "https://rickandmortyapi.com/api/character/?name=" + NameQuery
+    }
+
+
    var xhr = new XMLHttpRequest()
-   xhr.open('GET', 'https://rickandmortyapi.com/api/character/' )
-   xhr.send()
-   xhr.onload = function(){
-      if(xhr.status === 200){
-         var res = JSON.parse(xhr.response)
-         console.log(res)
-         AllCharacter(res)
-        
-      }
-      else{
-         var div = document.createElement('div')
-         div.textContent = "The Request has Failed. Please Try again Later!!"
-      }
-   }
-
-}
-
-function AllCharacter(data){
-   var items  = data.results
-   var displayResult = document.getElementById('charResult')
-   displayResult.setAttribute('class','row')
-   displayResult.textContent = ""
-
-   for(var i = 0 ; i < items.length ; i++){
-      var card = document.createElement('div')
-      card.setAttribute('class','card col-lg-6')
-
-      var cardRow = document.createElement('div')
-      cardRow.setAttribute('class','row no-gutters')
-
-      var cardCol = document.createElement('div')
-      cardCol.setAttribute('class','col-md-4 p-3')
-      var  img = document.createElement('img')
-      img.setAttribute('src',items[i].image)
-      img.setAttribute('class','card-img img-fluid')
-      
-      cardCol.append(img)
-
-      var cardBody = document.createElement('div')
-      cardBody.setAttribute('class','card-body col-md-8')
-
-      var namePara = document.createElement('p')
-      namePara.textContent = items[i].name
-      namePara.style.fontWeight = 'bold'
-
-      var DatePara = document.createElement('p')
-      DatePara.textContent = items[i].status  +"\n"+ "-"+"\n" +  items[i].species
-
-      
-      var genderPara = document.createElement('p')
-      genderPara.textContent =  items[i].gender
-
-      cardBody.append(namePara,DatePara,genderPara)
-      cardRow.append(cardCol,cardBody)
-      card.append(cardRow)
-      displayResult.append(card)     
-
-   }
-}
-
-function getSingleChar(){
-   var xhr = new XMLHttpRequest()
-   xhr.open('GET', 'https://rickandmortyapi.com/api/character/?name='+ query)
+   xhr.open('GET', url )
    xhr.send()
    xhr.onload = function(){
       if(xhr.status === 200){
@@ -130,7 +87,7 @@ function displaySingleChar(data){
       DatePara.textContent = items[i].status  +"\n"+ "-"+"\n" +  items[i].species
 
       var genderPara = document.createElement('p')
-      genderPara.textContent = items[i].status
+      genderPara.textContent = items[i].gender 
 
       cardBody.append(namePara,DatePara,genderPara)
       cardRow.append(cardCol,cardBody)
